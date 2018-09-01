96655e7cbddbc700ff066a8d29146d87c36af469
Christina <christin@njit.edu>
2018-09-01 09:26:15 -0400

    math comments

--- HMM_FEM_2d.m
+++ HMM_FEM_2d.m
@@ -2,14 +2,15 @@
 
 %% Quick and dirty HMM-FEM 2D solver
 % Want to solve $\nabla \cdot (A_HMM \nabla U_HMM) = f$ on $\Omega = [0, 1]\times
-% [0, 1]$, $U_HMM = g$ on $\partial \Omega$
+% [0, 1]$, $U_HMM = g$ on $\partial \Omega$ (Dirichlet) or $\nabla U_HMM
+% \cdot n = g$ (Neumann)
 
-%%
+%% macroscopic RHS d and BC rhs g
 pde.g           = '0'; 
 pde.f           = '1';
 
 
-%% a^epsilon and HMM optionz
+%% a^epsilon and HMM options
 epsilon     = 1/100;
 a           = @(x1, x2, y1, y2) 1.1 + sin(2*pi*y2);
 aeps        = @(x,y) a(x,y, x/epsilon, y/epsilon);
@@ -21,10 +22,10 @@ nMicro      = 30;
 nMacro                  = 15;
 
 load('g'); % I used pdetool to generate a square and saved it as the file 'g.mat';
-hmmopts     = struct('epsilon', epsilon, 'nMicro',nMicro, 'deltax',cellSize*epsilon,'deltay',cellSize*epsilon, 'cellSize', cellSize);
-%%
+hmmopts     = struct('epsilon', epsilon, 'nMicro',nMicro, 'deltax',cellSize*epsilon,'deltay',cellSize*epsilon, 'cellSize', cellSize, bc, 'Dirichlet');
+%% This takes forever, so do it one time and save the mesh as a .mat file.
 [macro_mesh, micro_mesh]=hmmmesh(nMacro,g, hmmopts);
-%%
+
 figure;
 pdemesh(micro_mesh.p, micro_mesh.e, micro_mesh.t); hold on;
 pdemesh(macro_mesh.p, macro_mesh.e, macro_mesh.t);
@@ -43,10 +44,10 @@ end
 
 
 
-%% Solve HMM
-[u, A_HMM] = myHMM(macro_mesh, micro_mesh, aeps,pde, hmmopts);
+%% Produce HMM solution and coefficent A_HMM at the macro_mesh midpoints
+[U_HMM, A_HMM] = myHMM(macro_mesh, micro_mesh, aeps,pde, hmmopts);
 figure;
-pdesurf(macro_mesh.p,macro_mesh.t,u);
+pdesurf(macro_mesh.p,macro_mesh.t,U_HMM);
 
 
 
